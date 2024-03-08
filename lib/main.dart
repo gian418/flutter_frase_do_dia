84dd2c48ee26091330efe6bf4d5b27f2ccd87fc6
Giancarlo Haack Albano <gian418@gmail.com>
2024-03-07 22:03:23 -0300

    Encerrando app frases do dia

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,90 @@
-import 'dart:ui';
+import 'dart:math';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main () {
   runApp(
-      MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: "Frases do dia",
-        home: Container(
-          // color: Colors.white,
-          padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
-          margin: const EdgeInsets.all(0),
-          // margin: const EdgeInsets.only(top: 30, left: 10),
-          // margin: const EdgeInsets.fromLTRB(0, 0, 0, 0),
-          decoration: BoxDecoration(
-            border: Border.all(width: 3, color: Colors.white)
-          ),
-          child: const Column(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    )
+  );
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  var _frases = [
+    "Imagine aqui uma frase motivacional 01",
+    "Imagine aqui uma frase motivacional 02",
+    "Imagine aqui uma frase motivacional 03",
+    "Imagine aqui uma frase motivacional 04",
+  ];
+
+  var _fraseGerada = "Clique abaixo para gerar uma frase!";
+
+  void _gerarFrase() {
+    var numeroSorteado = Random().nextInt(_frases.length);
+
+    setState(() {
+      _fraseGerada = _frases[numeroSorteado];
+    });
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Frases do dia"),
+        backgroundColor: Colors.green,
+      ),
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(16),
+          // width: double.infinity, é o mesmo que 100% da largura. Mas foi utilizado o Center()
+          /*decoration: BoxDecoration(
+              border: Border.all(width: 3, color: Colors.amber)
+          ),*/
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Text("t1"),
-              
-              Padding(
-                  padding: EdgeInsets.all(30),
-                  child: Text("t2"),
-              ),
-              
-              Text("t3")
-              /*Text(
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vel sem bibendum, lacinia risus sit amet, egestas arcu.",
+              Image.asset("images/logo.png"),
+              Text(
+                _fraseGerada,
                 textAlign: TextAlign.justify,
-              )*/
+                style: TextStyle(
+                    fontSize: 25,
+                    fontStyle: FontStyle.italic,
+                    color: Colors.black
+                ),
+              ),
+              ElevatedButton(
+                onPressed: _gerarFrase,
+                child: Text(
+                  "Nova frase",
+                  style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(Colors.green)
+                ),
+              )
             ],
           ),
         ),
-      )
-  );
+      ),
+    );
+  }
 }
